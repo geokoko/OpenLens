003372f4acbbeb3bb7a75dd80eb5a3d3d6cc0d4e
geokoko <geokoko3@outlook.com.gr>
2024-04-03 22:37:05 +0300

    Added new implementation for CNN model

--- models/src/emotion_cnn.py
+++ models/src/emotion_cnn.py
@@ -1,34 +1,67 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision.models as models
 
-class EmotionCNN(nn.Module):
-    def __init__(self, num_classes=7):
-        super(EmotionCNN, self).__init__()
-        # Load a pre-trained ResNet-18 model. Inittially, the model is trained on ImageNet dataset
-        self.resnet = models.resnet18(pretrained=True)
+class Deep_Emotion(nn.Module):
+    def __init__(self):
+        super(Deep_Emotion,self).__init__()
+        # Loading resnet model
+        self.resnet = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True)
         
-        self.resnet.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        
-        # Remove the fully connected layers of ResNet-18 and use it purely as a feature extractor
-        self.resnet = nn.Sequential(*list(self.resnet.children())[:-2])
-        
-        # Custom layers
-        self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc1 = nn.Linear(512, 256)  # ResNet-18 features to 256
-        self.fc2 = nn.Linear(256, num_classes)  # Final output layer
+        self.conv1 = nn.Conv2d(1,10,3)
+        self.conv2 = nn.Conv2d(10,10,3)
+        self.pool2 = nn.MaxPool2d(2,2)
 
+        self.conv3 = nn.Conv2d(10,10,3)
+        self.conv4 = nn.Conv2d(10,10,3)
+        self.pool4 = nn.MaxPool2d(2,2)
 
+        self.norm = nn.BatchNorm2d(10)
 
-    def forward(self, x):
-        # Feature extraction
-        x = self.resnet(x)
-        x = self.pool(x)
-        x = torch.flatten(x, 1)
-        
-        # Custom classifier
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, 0.25, training=self.training)
-        x = self.fc2(x)
+        self.fc1 = nn.Linear(810,50)
+        self.fc2 = nn.Linear(50,7)
+
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+
+        self.fc_loc = nn.Sequential(
+            nn.Linear(640, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 640)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
         return x
+
+    def forward(self,input):
+        out = self.stn(input)
+
+        out = F.relu(self.conv1(out))
+        out = self.conv2(out)
+        out = F.relu(self.pool2(out))
+
+        out = F.relu(self.conv3(out))
+        out = self.norm(self.conv4(out))
+        out = F.relu(self.pool4(out))
+
+        out = F.dropout(out)
+        out = out.view(-1, 810)
+        out = F.relu(self.fc1(out))
+        out = self.fc2(out)
+
+        return out
